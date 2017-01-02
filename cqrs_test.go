f01d07d85ed591b68b8d68ffd003b6b01547e1c9
John <yehohanan7@gmail.com>
2017-01-02 18:58:10 +0530

    use gvt

---
+++ cqrs_test.go
@@ -0,0 +1,13 @@
+package cqrs
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+func TestBooks(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Cqrs Suite")
+}
