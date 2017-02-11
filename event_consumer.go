5a7422320f077934fa8f14c9475d8e17f8f47d6d
John <yehohanan7@gmail.com>
2017-02-11 18:22:28 +0000

    event consumer

--- event_consumer.go
+++ event_consumer.go
@@ -1,11 +1,19 @@
 package cqrs
 
-import "reflect"
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"reflect"
+
+	"github.com/golang/glog"
+)
 
 const defaultOffset = 0
 
 //Consumes events from the command component
 type EventConsumer struct {
+	url          string
 	handlerClass interface{}
 	handlers     handlermap
 }
@@ -18,7 +26,59 @@ func (consumer *EventConsumer) send(event Event) {
 	}
 }
 
+func fetchJson(url string, data interface{}) error {
+	res, err := http.Get(url)
+	if err != nil {
+		glog.Error("Error while getting ", url, err)
+		return err
+	}
+
+	body, err = ioutil.ReadAll(res.Body)
+	if err != nil {
+		glog.Error("Error while reading the data ", err)
+		return err
+	}
+
+	err = json.Unmarshal(body, data)
+
+	if err != nil {
+		glog.Error("Error while decoding data ", err)
+		return err
+	}
+
+	return nil
+}
+
+func getFeed(url string) (JsonEventFeed, error) {
+	var feed = new(JsonEventFeed)
+	err := fetchJson(url, feed)
+	if err != nil {
+		return nil, err
+	}
+	return *feed, nil
+}
+
+func getEvent(url string) (EventEntry, error) {
+	var event *EventEntry
+	err := fetchJson(url, event)
+	if err != nil {
+		return nil, err
+	}
+	return *event, nil
+}
+
 func (consumer *EventConsumer) Start() error {
+	feed, err := getFeed(consumer.url)
+	if err != nil {
+		return err
+	}
+
+	for _, entry := range feed.Events {
+		event := getEvent(entry.Url)
+		if handler, ok := consumer.handlers[reflect.TypeOf(payload)]; ok {
+
+		}
+	}
 	return nil
 }
 
@@ -27,6 +87,6 @@ func (consumer *EventConsumer) Stop() error {
 }
 
 //Create new consumer
-func NewConsumer(url string, handlerClass interface{}, store OffsetStore) EventConsumer {
-	return EventConsumer{handlerClass, buildHandlerMap(handlerClass)}
+func NewEventConsumer(url string, handlerClass interface{}, store OffsetStore) EventConsumer {
+	return EventConsumer{url, handlerClass, buildHandlerMap(handlerClass)}
 }
--- event_consumer_test.go
+++ event_consumer_test.go
@@ -1,8 +1,12 @@
 package cqrs
 
 import (
+	"io/ioutil"
+	"net/http"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"gopkg.in/h2non/gock.v1"
 )
 
 type NewStarBorn struct {
@@ -34,10 +38,23 @@ var _ = Describe("Event Consumer", func() {
 	BeforeEach(func() {
 		offsetStore = NewInMemoryOffsetStore()
 		consumer = new(UniverseEventConsumer)
-		consumer.EventConsumer = NewConsumer("http://localhost:1212/events", consumer, offsetStore)
+		consumer.EventConsumer = NewEventConsumer("http://localhost:1212/events", consumer, offsetStore)
+	})
+
+	AfterEach(func() {
+		gock.Off()
 	})
 
 	It("Should consume events from another service", func() {
-		Expect("").To(Equal(""))
+		json, _ := ioutil.ReadFile("testdata/universe_events.json")
+		gock.New("http://localhost:1212").
+			Get("/events").
+			Reply(200).
+			JSON(string(json))
+
+		resp, _ := http.Get("http://localhost:1212/events")
+		body, _ := ioutil.ReadAll(resp.Body)
+		Expect(string(body)).To(Equal(""))
 	})
+
 })
