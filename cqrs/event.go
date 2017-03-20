aa776e0243a7d07635bd22cd1740637d308ae3d4
John <yehohanan7@gmail.com>
2017-03-20 20:22:37 +0000

    break event into metadata and payload

--- cqrs/event.go
+++ cqrs/event.go
@@ -6,22 +6,26 @@ import (
 	uuid "github.com/satori/go.uuid"
 )
 
+type EventMetaData struct {
+	Id               string `json:"id"`
+	OccuredAt        string `json:"occured_at"`
+	AggregateVersion int    `json:"aggregate_version"`
+	AggregateName    string `json:"aggregate_name"`
+}
+
 //Every action on an aggregate emits an event, which is wrapped and saved
 type Event struct {
-	Id               string      `json:"id"`
-	OccuredAt        string      `json:"occured_at"`
-	AggregateVersion int         `json:"aggregate_version"`
-	AggregateName    string      `json:"aggregate_name"`
-	Payload          interface{} `json:"payload"`
+	EventMetaData
+	Payload interface{} `json:"payload"`
 }
 
 //Create new event
 func NewEvent(aggregateName string, aggregateVersion int, payload interface{}) Event {
-	return Event{
+	metaData := EventMetaData{
 		Id:               uuid.NewV4().String(),
 		AggregateVersion: aggregateVersion,
 		AggregateName:    aggregateName,
 		OccuredAt:        time.Now().Format(time.ANSIC),
-		Payload:          payload,
 	}
+	return Event{metaData, payload}
 }
