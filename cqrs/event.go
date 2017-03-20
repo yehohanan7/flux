63560ce3804ac8146d9a06e06bb0d31157b87f17
John <yehohanan7@gmail.com>
2017-03-20 20:20:26 +0000

    cleanup

--- cqrs/event.go
+++ cqrs/event.go
@@ -9,19 +9,19 @@ import (
 //Every action on an aggregate emits an event, which is wrapped and saved
 type Event struct {
 	Id               string      `json:"id"`
-	Payload          interface{} `json:"payload"`
 	OccuredAt        string      `json:"occured_at"`
 	AggregateVersion int         `json:"aggregate_version"`
 	AggregateName    string      `json:"aggregate_name"`
+	Payload          interface{} `json:"payload"`
 }
 
 //Create new event
 func NewEvent(aggregateName string, aggregateVersion int, payload interface{}) Event {
 	return Event{
 		Id:               uuid.NewV4().String(),
-		Payload:          payload,
 		AggregateVersion: aggregateVersion,
 		AggregateName:    aggregateName,
 		OccuredAt:        time.Now().Format(time.ANSIC),
+		Payload:          payload,
 	}
 }
