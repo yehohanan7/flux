82f9d788dafd39ed78ed93d85c66a027c071cb0c
Vivek VK <vivektheone@gmail.com>
2017-03-30 07:31:53 +0530

    Added Vue based app

---
+++ examples/bank/gui/js/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <section class="header">
+        <h4 class="title">{{ msg }}</h4>
+      </section>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Bank App Example*'
+    }
+  }
+}
+</script>
+
